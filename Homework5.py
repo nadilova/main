f827b3c6ebb66be4df6fbc90a410a67e8035fd31
nadilova <nadilova@gmail.com>
2022-10-26 15:18:03 +0200

    Try number 3

--- Homework5.py
+++ Homework5.py
@@ -1,6 +1,11 @@
-message = 'Hello World!'
+def message = 'Hello World!'
 print(message)
 
-message1 = 'One more attempt'
+def message1 = 'One more attempt'
 print(message1)
+
+def x=5
+y=6
+resultat = x+y
+print(resultat)
 
